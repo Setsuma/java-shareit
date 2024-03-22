ff79c07055f5aacda10e42e415e52d2b79e909d0
Setsuma <pilipas.s.a@yandex.ru>
2024-03-22 19:51:25 +0300

    add-docker InProgress

--- src/main/java/ru/practicum/shareit/exception/IdNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class IdNotFoundException extends RuntimeException {
-    public IdNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/exception/UnavailableException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class UnavailableException extends RuntimeException {
-    public UnavailableException(String message) {
-        super(message);
-    }
-}
-
